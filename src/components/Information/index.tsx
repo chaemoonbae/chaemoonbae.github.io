105c08404ff7bf84d7f1a5daf2761a97907f5be3
chaemoonbae <mideas09@gmail.com>
2024-07-07 03:08:21 +0900

    자기소개 수정

--- src/components/Information/index.tsx
+++ src/components/Information/index.tsx
@@ -9,7 +9,8 @@ const Information = ({ information }: Pick<DataProps, "information">) => {
       <div className="flex flex-col gap-2">
         <h1 className="leading-[1.15] pb-20">
           {/* 포지션에 맞게 문구를 수정해주세요. 혹은, 본인이 원하는 대로 문구를 바꿔주세요. */}
-          <span className="text-PRIMARY font-semibold">{information.name}</span> 포트폴리오
+          <span className="font-semibold text-PRIMARY">{information.name}</span> 포트폴리오
+          <br /> 게시판과 이 포토폴리오의 css는 월요일 전에 꼭 수정하겠습니다..
         </h1>
         {/* <div className="flex gap-1">
           {information.contact.map((contact) => (
