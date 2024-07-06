263a147228b788942d1467a2f929469bdde7d089
chaemoonbae <mideas09@gmail.com>
2024-07-07 02:00:44 +0900

    설정 변경2

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH,
+  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
   webpack: (config) => {
     config.module.rules.push({
       test: /\.md/,
@@ -16,8 +18,3 @@ const nextConfig = {
 };
 
 module.exports = nextConfig;
-
-module.exports = {
-  basePath: process.env.NEXT_PUBLIC_BASE_PATH,
-  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
-}
