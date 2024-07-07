1c9d21fcf86d1eba918945cccc264812a0f25ee3
chaemoonbae <mideas09@gmail.com>
2024-07-08 05:51:35 +0900

    포트폴리오 수정

--- src/components/Certificate/CertificateItem.tsx
+++ src/components/Certificate/CertificateItem.tsx
@@ -1,12 +1,12 @@
 const CertificateItem = ({ skills }: { skills: string[] }) => {
   return (
-    <div className="flex flex-col md:flex-row gap-6 md:gap-0">
+    <div className="flex flex-col gap-6 md:flex-row md:gap-0">
       <div className="flex flex-col gap-2">
-        <div className="flex gap-4 flex-wrap">
+        <div className="flex flex-wrap gap-4">
           {skills.map((skill) => (
             <span
               key={skill}
-              className=" bg-PRIMARY dark:bg-white py-[2px] px-2 rounded-md text-xs font-medium font-mono whitespace-nowrap text-xl text-white dark:text-BLACK"
+              className=" bg-PRIMARY dark:bg-white py-[2px] px-2 rounded-md text-lg font-medium font-mono whitespace-nowrap text-xl text-white dark:text-BLACK"
             >
               {skill}
             </span>
