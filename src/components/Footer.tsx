c575c0bf96b9ce14f7eaf14059a213fa1a91b602
chatmoon3 <chatmoon3@gmail.com>
2024-07-15 06:58:41 +0900

    24.07.15.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -8,7 +8,7 @@ const Footer = ({
   name: string;
 }) => {
   return (
-    <footer className="flex flex-col gap-2 justify-center items-center bg-GRAY h-28 text-xs text-white dark:bg-GRAY_EXTRAHEAVY dark:text-GRAY_HEAVY">
+    <footer className="flex flex-col items-center justify-center gap-2 text-xs text-white bg-GRAY h-28 dark:bg-GRAY_EXTRAHEAVY dark:text-GRAY_HEAVY">
       {/* <div className="flex gap-1">
         {contact.map((contact) => (
           <ContactItem key={contact.id} {...contact}>
@@ -17,7 +17,7 @@ const Footer = ({
         ))}
       </div> */}
       <div className="flex flex-col items-center">
-        <span className="whitespace-pre-wrap text-center">{`Copyright 2023. ${name} All rights reserved.`}</span>
+        <span className="text-center whitespace-pre-wrap">{`Copyright 2024. ${name} All rights reserved.`}</span>
         <a target="_blank" rel="noreferrer" href="https://github.com/sjoleee/very-simple-portfolio">
           Powered by very-simple-portfolio
         </a>
